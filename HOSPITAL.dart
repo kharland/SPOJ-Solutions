8e1cad2eb24a8945d83fe97b72b1847564849652
Kendal Harland <kharland2011@gmail.com>
2015-03-03 17:59:02 -0600

    added pretty printing

--- HOSPITAL.dart
+++ HOSPITAL.dart
@@ -1,18 +1,23 @@
 /**
-SPOJ HOSPITAL problem
+* partial solution SPOJ HOSPITAL problem. This answer leaves out the table
+* indicating the total use time and usage amount for each operating table
+* and recovery room and the output of this program is not robust like the 
+* required challenge output. This is just a solution to the scheduling portion
+* of the algorithm.
+* 
 */
 
 import 'dart:io';
 
 const int NO_PATIENT = -1;
-const int OPERATING_TABLE_COUNT = 5;
-const int RECOVERTY_BED_COUNT = 5;
-const int _START_HOUR = 7;
-const int START_TIME = _START_HOUR*60;
-const int TRANSPORT_TIME = 5;
-const int OPERATING_TABLE_PREP_TIME = 15;
-const int RECOVERTY_BED_PREP_TIME = 10;
-const int TOTAL_PATIENTS = 16;
+int OPERATING_TABLE_COUNT = 5;
+int RECOVERTY_BED_COUNT = 5;
+int _START_HOUR = 7;
+int START_TIME = _START_HOUR*60;
+int TRANSPORT_TIME = 5;
+int OPERATING_TABLE_PREP_TIME = 15;
+int RECOVERTY_BED_PREP_TIME = 10;
+int TOTAL_PATIENTS = 16;
 
 class Patient {
   final String name;
@@ -20,6 +25,8 @@ class Patient {
   final int recoveryTime;
   int operationStart;
   int recoveryStart;
+  int table;
+  int bed;
   Patient(this.name, this.operationTime, this.recoveryTime);
 }
 
@@ -34,25 +41,46 @@ class HospitalRoom {
 
 List tables = new List<HospitalRoom>.generate(OPERATING_TABLE_COUNT, (_) => new HospitalRoom.fromOpenTime(START_TIME));
 List beds = new List<HospitalRoom>.generate(RECOVERTY_BED_COUNT, (_) => new HospitalRoom.fromOpenTime(START_TIME));
-List patients = [
-  new Patient("Jones", 28, 140),
-  new Patient("Smith", 120, 200),
-  new Patient("Thompson", 23, 75),
-  new Patient("Albright", 19, 82),
-  new Patient("Poucher", 133, 209),
-  new Patient("Comer", 74, 101),
-  new Patient("Perry", 93, 188),
-  new Patient("Page", 111, 223),
-  new Patient("Roggio", 69, 122),
-  new Patient("Brigham", 42, 79),
-  new Patient("Nute", 22, 71),
-  new Patient("Young", 38, 140),
-  new Patient("Bush", 26, 121),
-  new Patient("Cates", 120, 248),
-  new Patient("Johnson", 86, 181),
-  new Patient("White", 92, 140)
-];
+List patients = [];
 int nextPatient = 0;
+  // new Patient("Jones", 28, 140),
+  // new Patient("Smith", 120, 200),
+  // new Patient("Thompson", 23, 75),
+  // new Patient("Albright", 19, 82),
+  // new Patient("Poucher", 133, 209),
+  // new Patient("Comer", 74, 101),
+  // new Patient("Perry", 93, 188),
+  // new Patient("Page", 111, 223),
+  // new Patient("Roggio", 69, 122),
+  // new Patient("Brigham", 42, 79),
+  // new Patient("Nute", 22, 71),
+  // new Patient("Young", 38, 140),
+  // new Patient("Bush", 26, 121),
+  // new Patient("Cates", 120, 248),
+  // new Patient("Johnson", 86, 181),
+  // new Patient("White", 92, 140)
+
+void ParseHeader() {
+  List header = stdin.readLineSync().split(' ');
+  OPERATING_TABLE_COUNT = int.parse(header[0]);
+  RECOVERTY_BED_COUNT   = int.parse(header[1]);
+  _START_HOUR           = int.parse(header[2]);
+  START_TIME            = _START_HOUR*60;
+  TRANSPORT_TIME        = int.parse(header[3]);
+  OPERATING_TABLE_PREP_TIME = int.parse(header[4]);
+  RECOVERTY_BED_PREP_TIME   = int.parse(header[5]);
+  TOTAL_PATIENTS = int.parse(header[6]);
+}
+
+void ParsePatientList() {
+  for (int i=0; i<TOTAL_PATIENTS; i++) {
+    var name = stdin.readLineSync(),
+        times = stdin.readLineSync().split(' '),
+        operationTime = int.parse(times[0]),
+        recoveryTime  = int.parse(times[1]);
+    patients.add(new Patient(name, operationTime, recoveryTime));
+  }
+}
 
 int GetNextPatient() {
   nextPatient += 1;
@@ -89,6 +117,7 @@ void BeginPatientOperation(int patient) {
   }
   tables[earliest].patient = patient;
   patients[patient].operationStart = tables[earliest].openTime;
+  patients[patient].table = earliest+1;
   tables[earliest].openTime += patients[patient].operationTime;
   tables[earliest].openTime += OPERATING_TABLE_PREP_TIME;
 }
@@ -104,13 +133,14 @@ void MovePatientToBed(int patient) {
   patients[patient].recoveryStart = patients[patient].operationStart +
                                     patients[patient].operationTime +
                                     TRANSPORT_TIME;
+  patients[patient].bed = earliest+1;
   beds[earliest].patient = patient;
   beds[earliest].openTime += patients[patient].recoveryTime;
   beds[earliest].openTime += RECOVERTY_BED_PREP_TIME;
 }
 
 String _secsToMins(int sec) {
-  String minutes = "${(sec/60).floor()}".padLeft(2, "0"),
+  String minutes = "${(sec/60).floor()}",
          seconds = "${(sec % 60)}".padLeft(2, "0");
   return "$minutes:$seconds";
 }
@@ -127,15 +157,21 @@ void ComputeSchedule() {
 }
 
 main () {
+  ParseHeader();
+  ParsePatientList();
   ComputeSchedule();
-  for (var patient in patients) {
-      stdout.write(patient.name.padRight(10, ' '));
-      stdout.write(_secsToMins(patient.operationStart));
-      stdout.write("-");
-      stdout.write(_secsToMins(patient.operationStart + patient.operationTime));
-      stdout.write("   ");
-      stdout.write(_secsToMins(patient.recoveryStart));
-      stdout.write("-");
+  print("Patient          Operating Room          Recovery Room");
+  print("#  Name     Room#  Begin   End      Bed#  Begin    End");
+  print("------------------------------------------------------");
+  for (var i=0; i<patients.length; i++) {
+      var patient = patients[i];
+      stdout.write("${i+1}".padRight(3, ' '));
+      stdout.write(patient.name.padRight(11, ' '));
+      stdout.write("${patient.table}".padRight(5, ' '));
+      stdout.write(_secsToMins(patient.operationStart).padRight(8, ' '));
+      stdout.write(_secsToMins(patient.operationStart+patient.operationTime).padRight(10, ' '));
+      stdout.write("${patient.bed}".padRight(5, ' '));
+      stdout.write(_secsToMins(patient.recoveryStart).padRight(8, ' '));
       stdout.write(_secsToMins(patient.recoveryStart + patient.recoveryTime));
       stdout.writeln();
   }
